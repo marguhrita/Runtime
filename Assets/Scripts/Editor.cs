343e5068ce482b55017d8c9fd2a4c963445f9387
Alastair <82453045+marguhrita@users.noreply.github.com>
2025-10-23 11:57:43 +0100

    Split into Lexer and Parser now

--- Assets/Scripts/Editor.cs
+++ Assets/Scripts/Editor.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEditor.Rendering.BuiltIn.ShaderGraph;
 using UnityEngine;
 
 public class Editor : MonoBehaviour
@@ -57,20 +60,48 @@ public class Editor : MonoBehaviour
     }
 }
 
-public class Tokenizer
+public class Parsenizer
 {
     private string buf;
-
+    private List<Node> nodes;
     public void tokenize(string code)
     {
-        foreach (char c in code){
-            buf += c; // add new character to character buffer
+        int i = 0;
+
+        while (i <= code.Length)
+        {
+            buf += code[i]; // add new character to character buffer
 
             // Checks for things
+            // Variable check
             if (buf.ToLower() == "variable")
             {
+                string id = findIdentifier(code[(i + 1)..]);
                 
+                buf = "";
             }
+
+
+            // Function call check
+            else
+            {
+                string id = findIdentifier(code[i..]);
+            }
+
+
         }
+    }
+    
+    private string findIdentifier(string code)
+    {
+        int i = 0;
+        string identifier = "";
+        while (code[i] != ' ') // loop until a space is found 
+        {
+            identifier += code[i];
+            i += 1;
+        }
+
+        return identifier;
     }
 }
