81adeecc3619e88179ba36d84be8b371212805ac
Alastair <s2157894@ed.ac.uk>
2025-11-22 13:37:45 +0000

    Created GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _singleton;
+    public static GameManager Singleton
+    {
+        get => _singleton;
+        private set
+        {
+            if (_singleton == null)
+                _singleton = value;
+            else if (_singleton != value)
+            {
+                Debug.Log($"{nameof(gameObject)} instance already exists, destroying duplicate!");
+                Destroy(value);
+            }
+        }
+    }
+
+
+    // TODO make a disable movement method
+    void Start()
+    {
+        _singleton = this;
+    }
+
+}
