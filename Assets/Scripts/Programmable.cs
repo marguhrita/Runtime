403625643504cdc5f5d5715da867326d69921a3b
Alastair <s2157894@ed.ac.uk>
2025-11-22 16:26:15 +0000

    Folders Reshuffle

--- Assets/Scripts/Programmable.cs
+++ Assets/Scripts/Programmable.cs
@@ -1,15 +1,17 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Programmable : MonoBehaviour
 {
+    public List<Nodes
     void OnMouseDown()
     {
         Debug.Log("sigma nuts");
         
         Editor.Singleton.EditorUI.SetActive(true);
         
-
     }
 
 
