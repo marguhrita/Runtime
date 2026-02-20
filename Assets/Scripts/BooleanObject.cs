85622e6ca431cb5d685aea7a4148e137ec8520dc
Alastair <s2157894@ed.ac.uk>
2026-02-20 01:57:34 +0000

    True false topggle added for boolean object

--- Assets/Scripts/BooleanObject.cs
+++ Assets/Scripts/BooleanObject.cs
@@ -6,11 +6,36 @@ public class BooleanObject : MonoBehaviour
 {
     [SerializeField] public string Name;
     [SerializeField] public bool Value;
+    [SerializeField] private Material mat;
+    private Color lastColour;
+    private int colourID;
+
+
 
     void OnMouseDown()
     {
         Value = !Value;
         GameManager.Singleton.BoolDict[Name] = Value;
+    }
+
+    void Start()
+    {
+        colourID = Shader.PropertyToID("_BaseColor");
+        lastColour = ChooseTargetColour();
+
+    }
+
+    void Update()
+    {
+        // Change colour of the platform if state changed
+        Color nextColour = ChooseTargetColour();
+        if (lastColour != nextColour)
+        {
+            lastColour = nextColour;
+
+            mat.SetColor(colourID, nextColour);
+
+        }
     }
     
     private Color ChooseTargetColour()
