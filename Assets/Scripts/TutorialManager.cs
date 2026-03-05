6fae7bea672bda8fcb49c79f25ff6eb0817149bc
Alastair <s2157894@ed.ac.uk>
2026-03-05 22:17:39 +0000

    Camera Logic refactoring

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -6,7 +6,6 @@ using System.Data;
 using UnityEngine.UI;
 using Unity.Cinemachine;
 using System;
-using Unity.VisualScripting;
 using System.Collections;
 
 public class TutorialManager : MonoBehaviour
@@ -15,14 +14,19 @@ public class TutorialManager : MonoBehaviour
     private PlayerInput _playerInput;    
     [SerializeField] private RawImage progressionIndicator;
     [SerializeField] private float delay;
+    
+    [SerializeField] private int[] cameraSteps;
 
     [Header("Cameras")]
     public CinemachineCamera playerCamera;
-    public CinemachineCamera panTargetCamera;
+    public CinemachineCamera[] panTargetCamera; 
+    
+    private int cameraCount;
     private int currentStep;
     [SerializeField] private float cameraPanSpeed;
 
-
+    private bool isPanning = false;
+    private CinemachineCamera activePanCamera;
 
     public void TriggerEntered(int CutsceneNumber)
     {
@@ -36,12 +40,38 @@ public class TutorialManager : MonoBehaviour
     {
         DialogueManager.Instance.TextPanel.OnTextDoneIterating += StepDone;
         currentStep = 0;
+        cameraCount = 0;
+    }
+
+    void Update()
+    {
+        if (isPanning && activePanCamera != null)
+        {
+            var orbitalFollow = activePanCamera.GetComponent<CinemachineOrbitalFollow>();
+            if (orbitalFollow != null)
+            {
+                orbitalFollow.HorizontalAxis.Value += Time.deltaTime * cameraPanSpeed;
+            }
+        }
     }
 
     public void NextTutorialStep()
     {
         progressionIndicator.enabled = false;
         currentStep += 1;
+
+        if (cameraCount < cameraSteps.Length && currentStep == cameraSteps[cameraCount])
+        {
+            // Activate the corresponding camera
+            activePanCamera = panTargetCamera[cameraCount];
+            activePanCamera.Priority = 15;
+            
+            // Start the panning in the Update loop
+            isPanning = true;
+            
+            // Move to the next camera in the list for the next time
+            cameraCount++; 
+        }
     }
 
     public IEnumerator showIndicator()
@@ -50,24 +80,18 @@ public class TutorialManager : MonoBehaviour
         progressionIndicator.enabled = true;
     }
 
-    void Update()
-    {
-        if (currentStep == 2)
-        {
-            panTargetCamera.Priority = 15;
-            panCamera(panTargetCamera);
-        }
-    }
-
-    void panCamera(CinemachineCamera cam)
-    {
-        cam.GetComponent<CinemachineOrbitalFollow>().HorizontalAxis.Value += Time.deltaTime * cameraPanSpeed;
-    }
-
     void StepDone()
     {
-        panTargetCamera.Priority = 0;
+        // Stop the camera from spinning once the text finishes
+        isPanning = false; 
+
+        // Lower the priority of the camera we just used so it blends back to the player
+        if (cameraCount > 0 && activePanCamera != null)
+        {
+            activePanCamera.Priority = 0;
+            activePanCamera = null;
+        }
+        
         StartCoroutine(showIndicator());
     }
-
 }
